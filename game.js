18021c785c18f03a266c40fa7a2b143f68590d5f
Andrew <lite.nite628@gmail.com>
2024-12-11 22:36:06 -0500

    Clamp player between certain y values

--- game.js
+++ game.js
@@ -7,8 +7,7 @@
 'use strict';
 
 let _FOTL = {};
-//gravity = -0.008;
-gravity = -1;
+gravity = 0;
 ///////////////////////////////////////////////////////////////////////////////
 function gameInit()
 {
@@ -17,7 +16,7 @@ function gameInit()
     
     _FOTL.player = new EngineObject(vec2(0, 0), vec2(2,1));
     _FOTL.player.color = (new Color).setHex('#bbbbbb');
-  _FOTL.player.gravityScale = .003;
+    _FOTL.player.gravityScale = .003;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -25,18 +24,22 @@ function gameUpdate()
 {
     // called every frame at 60 frames per second
     // handle input and update the game state
-    console.debug(keyIsDown('KeyW'))
-    if (keyIsDown('KeyW')) {
-      console.log('flap')
-      const adjustment = 1.5 * _FOTL.player.size.y;
-      console.log(mainCanvasSize)
-      console.log({pp: _FOTL.player.pos, adj: adjustment, npp: _FOTL.player.pos.y + adjustment})
-      let newPosY = _FOTL.player.pos.y + adjustment;
-      _FOTL.player.pos.y = newPosY;
-    }
+    let y = _FOTL.player.pos.y;
 
-    //if (_FOTL.player.pos.y < 10) _FOTL.player.pos.y = 10;
-    //if (_FOTL.player.pos.y + 10 >= mainCanvasSize.y) _FOTL.player.pos.y = mainCanvasSize.y;
+    const adjustment = 1.5 * _FOTL.player.size.y;
+    const minVal = -8;
+    const maxVal = 8;
+    const range = maxVal - minVal;
+
+    const step = range / 60;
+    const sign = (keyIsDown('KeyW') || keyIsDown('ArrowUp')) ? 1 : -1;
+    
+    y += sign * step;
+
+    if (y > maxVal) y = maxVal;
+    else if (y <= minVal) y = minVal;
+
+    _FOTL.player.pos.y = y;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
